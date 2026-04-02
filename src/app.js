ce49ec0a19e87ef01c1f9ae26f389d7f21c66c5d
Pontax02 <ppontanilla07@gmail.com>
2026-04-02 16:20:22 +0200

    feat(app): reorganize middleware setup and include validation routes

--- src/app.js
+++ src/app.js
@@ -3,19 +3,21 @@ import helmet from "helmet";
 import { corsOptions } from "./config/cors.js";
 import { rateLimitOptions } from "./config/rateLimit.js";
 import { errorHandler, notFoundHandler } from "./middlewares/error.middleware.js";
+import validateRoutes from "./routes/validate.routes.js";
+
+
+
 
 export const app = express();
 
 
 
 app.use(helmet());
-
 app.use(corsOptions);
-
-
 app.use(rateLimitOptions);
-
 app.use(express.json({ limit: `10mb` }));
+
+app.use('/api', validateRoutes);
 
 app.use(notFoundHandler);
 
